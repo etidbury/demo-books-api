c9caec1f700086f1960efebc1ca2f1cc1a4085a1
Edd Tidbury <edd.tidbury@gmail.com>
2024-03-25 21:18:57 +0000

    fix(ci): reuse existing server in e2e workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,6 +72,6 @@ export default defineConfig({
   webServer: {
     command: "pnpm run dev",
     url: "http://127.0.0.1:3000",
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 });
